3f02fe97c2dc38c72497a1cc81cca926df475d74
2023213936 <2023213936@student.uitm.edu.my>
2025-11-17 23:05:06 +0800

    Update AboutActivity.java

--- app/src/main/java/com/example/zakatpaymentapplication/AboutActivity.java
+++ app/src/main/java/com/example/zakatpaymentapplication/AboutActivity.java
@@ -1,9 +1,12 @@
 package com.example.zakatpaymentapplication;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +18,13 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about);
+
+        Toolbar myToolbar = findViewById(R.id.zakat_toolbar);
+        setSupportActionBar(myToolbar);
+
+        TextView link = findViewById(R.id.textView3);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
